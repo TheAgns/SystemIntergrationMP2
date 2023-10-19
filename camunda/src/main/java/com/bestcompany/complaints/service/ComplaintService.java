4775e58f33ffb968705bb1b7b6ce00a1774c772c
Theagns <markus.agnsgaard@hotmail.com>
2023-10-19 13:51:15 +0200

    pom

--- camunda/src/main/java/com/bestcompany/complaints/service/ComplaintService.java
+++ camunda/src/main/java/com/bestcompany/complaints/service/ComplaintService.java
@@ -1,4 +1,4 @@
-package com.yourcompany.complaints.service;
+package bestcompany.complaints.service;
 
 import org.springframework.stereotype.Service;
 
--- camunda/src/main/java/com/bestcompany/complaints/service/LoggingService.java
+++ camunda/src/main/java/com/bestcompany/complaints/service/LoggingService.java
@@ -1,10 +1,11 @@
-package com.yourcompany.complaints.service;
+package bestcompany.complaints.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-@Service
+import java.util.logging.Logger;
+
 public class LoggingService {
 
     private final Logger logger = LoggerFactory.getLogger(LoggingService.class);
